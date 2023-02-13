d61f27488963eb834b0f86144aae3eab83717161
bekOm <omurzakov.bek@bk.ru>
2023-02-13 21:42:50 +0600

    firstCommit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+function App() {
+  return (
+    <div className="w-4/5 text-center bg-orange-300 h-screen hover:bg-blue-500 transition-all">
+     <h1 className="text-red-500">Hello Tailwind</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/index.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from "react";
+
+interface HeaderProps {
+    logo: string
+}
+
+const Header: FC<HeaderProps> = ({ logo }) => {
+    return <div>Header</div>
+};
+
+export default Header;
